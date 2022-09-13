326dae83fbe8a7c4abcb62be51e06fb73aff84da
sangay-khandu <113422991+sangay-khandu@users.noreply.github.com>
2022-09-13 14:51:37 +0600

    Add files via upload

---
+++ area_of_circle.py
@@ -0,0 +1,11 @@
+# Finding area of a circle
+# formula = 3.14159 * r**2
+
+pi = 3.14
+
+radius = int(input("Please enter the radius : "))
+
+Area =  pi * radius ** 2
+
+print("The area of a circle is : ", Area)
+
